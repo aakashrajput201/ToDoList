98dde86ab015ae000e63183f9e8f9121d6b78e34
Aakash Rajput <aakashrajput201@gmail.com>
2021-06-21 16:10:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import TodoItem from "./TodoItem";
 
 function App() {
+  const[items,setItems]=useState([]);
+  const[inputText,setInputText]=useState("");
+
+  function handleChange(event){
+    setInputText(event.target.value);
+  }
+
+  function handleSubmit(event){
+    setItems((prevItems)=>{
+     return [...prevItems,inputText];
+    })
+    setInputText("");
+  }
+  function deleteItem(id){
+    setItems((prevItems)=>{
+      return prevItems.filter(
+        (item,index)=>{
+          return index !==id;
+        }
+      )
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+      <h1>To-Do-List</h1>
+      </div>
+      <div className="form">
+      <input onChange={handleChange} type="text" value={inputText}/>
+      <button onClick={handleSubmit}><span>Add Item</span></button>
+      </div>
+    <div>
+      <ul>
+      {items.map((item,index)=>{
+      return <TodoItem key={index} id={index} text={item} onChecked={deleteItem}/>
+      })}
+      </ul>
+      </div>
     </div>
+    
   );
 }
 
